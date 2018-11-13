1d14e3ffa0e8508936055f2ef1b9a07de4e631df
msminsu <msminsu@gmail.com>
2018-11-14 01:34:07 +0900

    ff

--- javascript-youtube/app.js
+++ javascript-youtube/app.js
@@ -19,7 +19,51 @@ $(document).ready(function(){
         function  loadVids(){
             $.getJSON(URL, options, function(data){
                 console.log(data);
+                var id = data.items[0].snippet.resourceId.videoId;
+                mainVid(id);
+                resultsLoop(data); 
+                
             })
         }
+
+
+        function mainVid(id){
+            $('#video').html(`
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            `);
+        }
+
+        function resultsLoop(data){
+
+            
+
+
+            $.each(data.items, function(i,item){
+                var thumb = item.snippet.thumbnails.medium.url;
+                var title = item.snippet.title;
+                var desc = item.snippet.description.substring(0, 100);
+                var vid = item.snippet.resourceId.videoId;
+
+
+
+
+                $('main').append(`
+                <article class="item" data-key="${vid}">
+                    <img src="${thumb}" alt=""  class="thumb">
+                    <div class="details">
+                        <h4>${title}</h4>
+                        <p>${desc}</p>
+                    </div>
+                </article>
+                `);
+
+            });
+        }
+
+        $('main').on('click','article',function(){
+            var id = $(this).attr('data-key');
+            mainVid(id);
+        });
+        
 
 });
